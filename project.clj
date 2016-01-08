548dbe014fa8b6a622a8ea88a67ef4aa20b433cc
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-08 00:23:35 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject luminus-http-kit "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "HTTP Kit adapter for Luminus"
+  :url "https://github.com/luminus-framework/luminus-http-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
