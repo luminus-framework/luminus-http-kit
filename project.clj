d53c77b67e705c7926bf2ebe6e40a75a09631e00
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-07-12 08:51:59 -0400

    bumped up http-kit to 2.2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus-http-kit "0.1.3"
+(defproject luminus-http-kit "0.1.4"
   :description "HTTP Kit adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-http-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [http-kit "2.1.19"]])
+                 [http-kit "2.2.0"]])
