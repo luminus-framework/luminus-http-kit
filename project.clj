94c799023bccdbc1469b8afec45bc92a988b9ece
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-08-12 10:30:32 -0400

    fix for stopping http-kit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-http-kit "0.1.7"
+(defproject luminus-http-kit "0.1.8"
   :description "HTTP Kit adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-http-kit"
   :license {:name "Eclipse Public License"
--- src/luminus/http_server.clj
+++ src/luminus/http_server.clj
@@ -7,11 +7,16 @@
     (log/info "starting HTTP server on port" port)
     (http-kit/run-server
       handler
-      (dissoc opts :handler :init))
+      (-> opts
+          (assoc  :legacy-return-value? false)
+          (dissoc :handler :init)))
     (catch Throwable t
       (log/error t (str "server failed to start on" host "port" port))
       (throw t))))
 
-(defn stop [http-server]
-  (@http-server :timeout 100)
-  (log/info "HTTP server stopped"))
+(defn stop 
+  ([http-server] (stop http-server 100))
+  ([http-server timeout]
+   (let [result @(future (http-kit/server-stop! http-server {:timeout timeout}))]
+     (log/info "HTTP server stopped")
+     result)))
